c704e37c13fbd780ab0fab7621248ae39591132d
Alejo47 <alejojcr_1999@hotmail.com>
2017-08-10 14:49:32 -0300

    Added bootstrap gem to the gemfile and reconfigured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'eventos/listaev'
-  get 'eventos/extensionev'
+  match '/eventos', to: 'eventos#listaev', via: :get
+  match '/eventos/:id', to: 'eventos#extensionev', via: :get
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'eventos#listaev'
