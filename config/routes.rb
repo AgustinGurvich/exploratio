b41cde95cfcc6b27040479d5048522d4617c58ef
Alejo47 <alejojcr_1999@hotmail.com>
2017-08-10 15:00:55 -0300

    Reconfigured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'eventos/listaev'
-  get 'eventos/extensionev'
-
+  
+  get '/eventos/' => "eventos#listaev"
+  get '/eventos/:id' => 'eventos#extensionev'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'eventos#listaev'
 end
