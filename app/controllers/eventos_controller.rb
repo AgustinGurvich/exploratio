a871c54a84d46c4ad2fc72a2bcebe818f7cdad0c
Alejo47 <alejojcr_1999@hotmail.com>
2017-08-23 16:37:58 -0300

    Operational DetEv page

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -21,6 +21,23 @@ class EventosController < ApplicationController
     @Json = JSON.parse(resp.body)
     if @Json[0] == nil
       render html: "Error: 404: Not Found"
+    else
+      @Json = @Json[0]
+      @Title = @Json["title"]
+      @Desc = @Json["texto"]
+      @ImgUri = @Json["uri_imagen"]
+      @Date = @Json["fecha_y_hora_inicio"]
+      if @Json["lugares"].length == 0
+        @Location = nil
+      else
+        @Location = {
+          "name" => @Json["lugares"][0]["nombre"],
+          "full_addres" => @Json["lugares"][0]["direccion_completa"],
+          "coordx" => @Json["lugares"][0]["coordenada_x"],
+          "coordy" => @Json["lugares"][0]["coordenada_y"]
+        } 
+      end
+      @WebSite = @Json["link"]
     end
   end
 end
